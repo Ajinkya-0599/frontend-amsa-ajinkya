ddd4798e1a88118a427c2e67edfd4510b377262b
Ajinkya-0599 <ajinkya0599@gmail.com>
2025-10-03 16:00:57 +0530

    Test

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -84,7 +84,7 @@ export default function ContactPage() {
    
       // const backendApiUrl = process.env.NEXT_PUBLIC_API_URL;
       // const backendApiUrl = "http://localhost:3001";
- const backendApiUrl = "http://13.49.64.169/";     
+ const backendApiUrl = "http://13.49.64.169/";      
 
      
       if (!backendApiUrl) {
